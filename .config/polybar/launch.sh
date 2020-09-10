efaf0b373ce0f8861933569e20a47e21509217f6
Daniel Bell <dan.g.bell@gmail.com>
2020-09-10 18:37:32 +1200

    Changed config and launch script so it handled multiple monitors like I want it to

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,6 +4,8 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep; done
 
-polybar mybar &
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+	MONITOR=$m polybar --reload mybar &
+done
 
 echo "Polybar launched.."
